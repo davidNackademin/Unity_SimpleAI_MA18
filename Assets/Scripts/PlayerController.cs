c58287ca3ba36a02a7e1130bf3bbd403b6a6b219
David Svensson <davidsvson@gmail.com>
2019-09-27 10:27:25 +0200

    added player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector2 moveVelocity;
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 distanceToMove = moveVelocity * Time.fixedDeltaTime;
+        body.MovePosition(body.position + distanceToMove);
+    }
+
+}
